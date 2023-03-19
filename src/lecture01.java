5ff51012b4e99bc2066470c9033f86c72c2d1df8
Ashley <ashleysahn11@gmail.com>
2023-03-19 14:11:39 +0900

    capybara

---
+++ src/lecture01.java
@@ -0,0 +1,23 @@
+public class lecture01 {
+    public static void main(String[] args) {
+
+        // 정수형 (기본)
+        byte number1 = 1;
+        short number2 = 2;
+        int number3 = 3;
+        long number4 = 31;
+
+        // 실수형
+        float fNumber = 11f;
+        double dNumber = 22.0;
+
+        // 문자형
+        char character ='A';
+
+        // boolean형
+        boolean flag = true;
+
+        // 창조형
+        String str = "Hello World"
+    }
+}
