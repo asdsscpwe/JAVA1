a932625ef949fa41448d1ed3fe9fb7404fa50ed5
Ashley <ashleysahn11@gmail.com>
2023-05-14 12:28:03 +0900

    ddwdefㅈㄷㄹㄷㅈㄹㅈㄹ

---
+++ src/homework4/Ex4_17.java
@@ -0,0 +1,9 @@
+package src.homework4;
+
+public class Ex4_17 {
+    for(int i=0; i <= 10; i++) {
+        if (i%3==0)
+            continue;
+        System.out.println(i);
+    }
+}
---
+++ src/homework4/Ex4_7.java
@@ -0,0 +1,10 @@
+package src.homework4;
+
+public class Ex4_7 {
+    int num = 0;
+
+    for (int i = 1; i <= 5; i++) {
+        num = (int) (Math.random() * 6) +1;
+        System.out.println(num);
+    }
+}
